c9f4628ce9ee3c7437c3ee622484cb75fe91e6a3
Paul Fresnais <163826999+PaulFresnais@users.noreply.github.com>
2024-09-24 13:59:04 +0200

    Add files via upload
    Add information about monitor and corrections about some commands.

--- recupere_donnees_v2.ps1
+++ recupere_donnees_v2.ps1
@@ -2,21 +2,19 @@
 Install-Module -Name ImportExcel -Scope CurrentUser
 
 # ----- Nom de l'utilisateur -----
-
 $UserInfo = Get-CimInstance Win32_Process -Filter 'name="explorer.exe"' | Invoke-CimMethod -MethodName GetOwner
 $UserName = $UserInfo.User
 
 # ------ BIOS -----
-
 $Bios = Get-CimInstance -ClassName Win32_BIOS
 $BiosCaption = $Bios.Caption
 $BiosVersion = $Bios.Version
 $BiosSMBIOS = $Bios.SMBIOSMajorVersion
 $BiosSystemBiosVersion = $Bios.SystemBiosMajorVersion
 $BiosReleaseDate = $Bios.ReleaseDate
+$BiosSerialNumber = $Bios.SerialNumber
 
 # ----- Processeur -----
-
 $Processor = Get-CimInstance -ClassName Win32_Processor
 $ProcessorName = $Processor.Name
 $ProcessorCaption = $Processor.Caption
@@ -25,7 +23,6 @@ $ProcessorNumberOfCores = $Processor.NumberOfCores
 $ProcessorSocketDesignation = $Processor.SocketDesignation
 
 # ----- Carte mère -----
-
 $MotherBoard = Get-CimInstance -ClassName Win32_BaseBoard
 $MotherBoardManufacturer = $MotherBoard.Manufacturer
 $MotherBoardProduct = $MotherBoard.Product
@@ -57,14 +54,12 @@ $MemoryType = $MemoryInstance | Select-Object @{
 }
 
 # ----- Carte Graphique -----
-
 $GraphicsInstance = Get-CimInstance -ClassName CIM_VideoController
 $GraphicsName = $GraphicsInstance.Name
 $GraphicsDriverVersion = $GraphicsInstance.DriverVersion
 $GraphicsVRAM = ($GraphicsInstance.AdapterRAM / 1GB).ToString('F2')
 
 # ----- Disque dur -----
-
 $DiskInstance = Get-CimInstance -Class Win32_LogicalDisk -Filter "DriveType=3"
 $DiskDeviceID = $DiskInstance.DeviceID
 $DiskVolumeName = $DiskInstance.VolumeName
@@ -73,13 +68,11 @@ $DiskUsedSpace = ((($DiskInstance.Size - $DiskInstance.FreeSpace) / 1GB).ToStrin
 $DiskTotalSpace = ($DiskInstance.Size / 1GB).ToString('F2')
 
 # ----- OS / système -----
-
 $OSInstance = Get-CimInstance -ClassName CIM_OperatingSystem
 $OSCaption = $OSInstance.Caption
 $OSVersion = $OSInstance.Version
 $OSInstallDate = $OSInstance.InstallDate
 $OSArchitecture = $OSInstance.OSArchitecture
-$OSCSName = $OSInstance.CSName
 $OSWindowsDirectory = $OSInstance.WindowsDirectory
 $OSNumberOfUsers = $OSInstance.NumberOfUsers
 $OSBootDevice = $OSInstance.BootDevice
@@ -91,16 +84,43 @@ $SystemModel = $SystemInstance.Model
 $SystemManufacturer = $SystemInstance.Manufacturer
 
 # ----- Etat de santé des disques -----
-
 $disksHealth = Get-PhysicalDisk | Select-Object MediaType, OperationalStatus, HealthStatus
 
 # ----- Réseaux ------
-
-$networkAdapters = Get-CimInstance -ClassName CIM_NetworkAdapter | Select-Object Name, MACAddress, Speed
+$networkConfig = Get-CimInstance Win32_NetworkAdapterConfiguration -Filter "DHCPEnabled = true"
+$networkConnection = Get-CimInstance Win32_NetworkConnection
 
 # ----- Mise à jour OS -----
-
 $Maj = Get-CimInstance -ClassName Win32_QuickFixEngineering
+
+# ----- Ecrans d'ordinateur -----
+
+# Fonction de décodage des infos moniteur
+function Decode {
+    If ($args[0] -is [System.Array]) {
+        [System.Text.Encoding]::ASCII.GetString($args[0])
+    }
+    Else {
+        "Not Found"
+    }
+}
+
+# Récupération des informations du contrôleur vidéo /!\
+$videoController = Get-WmiObject Win32_VideoController
+
+#Info moniteur
+ForEach ($Monitor in Get-WmiObject WmiMonitorID -Namespace root\wmi) {
+    $Manufacturer = Decode $Monitor.ManufacturerName
+    $Name = Decode $Monitor.UserFriendlyName
+    $Serial = Decode $Monitor.SerialNumberID
+    $ManufactureYear = $Monitor.YearOfManufacture
+    $HorizontalResolution = $videoController.CurrentHorizontalResolution
+    $VerticalResolution = $videoController.CurrentVerticalResolution
+    $RefreshRate = $videoController.CurrentRefreshRate
+    $DriverVersion = $videoController.DriverVersion
+    $DriverDate = $videoController.DriverDate
+}
+
 
 # ----- Logiciels ------
 
@@ -125,6 +145,7 @@ $CombinedData = @(
     [PSCustomObject]@{ Property = "BIOS SMBIOS Version"; Value = $BiosSMBIOS }
     [PSCustomObject]@{ Property = "BIOS System BIOS Version"; Value = $BiosSystemBiosVersion }
     [PSCustomObject]@{ Property = "BIOS Release Date"; Value = $BiosReleaseDate }
+    [PSCustomObject]@{ Property = "Numéro de série"; Value = $BiosSerialNumber }
     [PSCustomObject]@{ Property = "Processeur Name"; Value = $ProcessorName }
     [PSCustomObject]@{ Property = "Processeur Caption"; Value = $ProcessorCaption }
     [PSCustomObject]@{ Property = "Processeur Max Clock Speed"; Value = $ProcessorMaxClockSpeed }
@@ -149,7 +170,6 @@ $CombinedData = @(
     [PSCustomObject]@{ Property = "Version de l'OS"; Value = $OSVersion }
     [PSCustomObject]@{ Property = "Date d'Installation"; Value = $OSInstallDate }
     [PSCustomObject]@{ Property = "Architecture de l'OS"; Value = $OSArchitecture }
-    [PSCustomObject]@{ Property = "Nom de l'Ordinateur"; Value = $OSCSName }
     [PSCustomObject]@{ Property = "Dossier Windows"; Value = $OSWindowsDirectory }
     [PSCustomObject]@{ Property = "Nombre d'Utilisateurs"; Value = $OSNumberOfUsers }
     [PSCustomObject]@{ Property = "Périphérique de Démarrage"; Value = $OSBootDevice }
@@ -159,18 +179,33 @@ $CombinedData = @(
     [PSCustomObject]@{ Property = "Fabricant Système"; Value = $SystemManufacturer }
 )
 
+# ----- Données Réseaux -----
+
+# ----- Données écrans -----
+$CombinedDataMonitor = @(
+    [PSCustomObject]@{ Property = "Fabricant"; Value = $Manufacturer }
+    [PSCustomObject]@{ Property = "Nom"; Value = $Name }
+    [PSCustomObject]@{ Property = "Numéro de série"; Value = $Serial }
+    [PSCustomObject]@{ Property = "Année de fabrication"; Value = $ManufactureYear }
+    [PSCustomObject]@{ Property = "Résolution horizontal"; Value = $HorizontalResolution }
+    [PSCustomObject]@{ Property = "Résolution vertical"; Value = $VerticalResolution }
+    [PSCustomObject]@{ Property = "Taux de rafraichissement"; Value = $RefreshRate }
+    [PSCustomObject]@{ Property = "Version du driver"; Value = $DriverVersion }
+    [PSCustomObject]@{ Property = "Date dernière mise à jour driver"; Value = $DriverDate }
+)
+
 # ----- Chemin du fichier Excel -----
 $excelFilePath = "Y:\FRESNAIS\Fiche utilisateurs\Fiche_$UserName.xlsx"
 
 # ----- Exportation des données -----
 $CombinedData | Export-Excel -Path $excelFilePath -WorksheetName "Données Système" -AutoSize
 $disksHealth | Export-Excel -Path $excelFilePath -WorksheetName "Etat de santé des disques" -AutoSize -Append
-$networkAdapters | Export-Excel -Path $excelFilePath -WorksheetName "Données réseaux" -AutoSize -Append
+$networkConfig | Export-Excel -Path $excelFilePath -WorksheetName "Configuration accès réseaux" -AutoSize -Append
+$networkConnection | Export-Excel -Path $excelFilePath -WorksheetName "Accès serveur" -AutoSize -Append
 $Maj | Export-Excel -Path $excelFilePath -WorksheetName "Mise à jour système" -AutoSize -Append
+$CombinedDataMonitor | Export-Excel -Path $excelFilePath -WorksheetName "Ecrans" -AutoSize -Append
 $installedSoftware | Export-Excel -Path $excelFilePath -WorksheetName "Données logiciel" -AutoSize -Append
 
-# Exportation des données initiales
-$CombinedData | Export-Excel -Path $excelFilePath -WorksheetName "Données Système" -AutoSize
 
 # ----------- Modification de l'alignement des cellules avec COM objets -----------
 
@@ -181,14 +216,25 @@ $Excel.Visible = $False  # False = invisible / true = Visible
 # Ouvrir le fichier Excel
 $Workbook = $Excel.Workbooks.Open($excelFilePath)
 
+# ---- Appliquer l'alignement à gauche sur la feuille "Données Système" ----
 # Sélectionner la feuille "Données Système"
-$Worksheet = $Workbook.Sheets.Item("Données Système")
+$Worksheet1 = $Workbook.Sheets.Item("Données Système")
 
 # Sélectionner toute la feuille
-$UsedRange = $Worksheet.UsedRange
+$UsedRange1 = $Worksheet1.UsedRange
 
 # Appliquer un alignement à gauche
-$UsedRange.HorizontalAlignment = -4131 #(Alignement à gauche)
+$UsedRange1.HorizontalAlignment = -4131 #(Alignement à gauche)
+
+# ---- Appliquer l'alignement à gauche sur la feuille "Écrans" ----
+# Sélectionner la feuille "Écrans"
+$Worksheet2 = $Workbook.Sheets.Item("Ecrans")
+
+# Sélectionner toute la feuille
+$UsedRange2 = $Worksheet2.UsedRange
+
+# Appliquer un alignement à gauche
+$UsedRange2.HorizontalAlignment = -4131 #(Alignement à gauche)
 
 # Sauvegarder et fermer le fichier Excel
 $Workbook.Save()
@@ -198,6 +244,9 @@ $Workbook.Close()
 $Excel.Quit()
 
 # Libérer la mémoire des objets COM
-[System.Runtime.Interopservices.Marshal]::ReleaseComObject($Worksheet) | Out-Null
+[System.Runtime.Interopservices.Marshal]::ReleaseComObject($UsedRange1) | Out-Null
+[System.Runtime.Interopservices.Marshal]::ReleaseComObject($UsedRange2) | Out-Null
+[System.Runtime.Interopservices.Marshal]::ReleaseComObject($Worksheet1) | Out-Null
+[System.Runtime.Interopservices.Marshal]::ReleaseComObject($Worksheet2) | Out-Null
 [System.Runtime.Interopservices.Marshal]::ReleaseComObject($Workbook) | Out-Null
 [System.Runtime.Interopservices.Marshal]::ReleaseComObject($Excel) | Out-Null
